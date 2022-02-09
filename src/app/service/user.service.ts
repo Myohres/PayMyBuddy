fc11b1b0865136c81b1283985fe5a3fec52da780
Myohres <77498044+Myohres@users.noreply.github.com>
2022-02-09 20:25:24 +0100

    adding subscription

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -25,6 +25,31 @@ export class UserService {
     return this.http.get<User>(this.rootURL+email);
   }
 
+  public loginTaken(email: string): Observable<boolean> {
+    return this.http.get<boolean>(this.rootURL+"taken/" +email)
+  }
+
+  public register(email: string, user: User){
+    this.loginTaken(email).subscribe({
+      next: value => {
+        console.log("login taken : " +value);
+        if (!value){
+          console.log("adding user ");
+          this.addUser(user).subscribe({
+            complete:() =>console.log("user added")
+            });
+        }},
+      error: err => console.error("error" +err),
+      complete:() => console.log("fin de registration")
+    });
+
+  }
+
+  public addUser(user: User): Observable<User> {
+    return  this.http.post<User>(this.rootURL, user)
+
+  }
+
   getUserId() {
     return this.userId;
   }
