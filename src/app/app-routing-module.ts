98422380aedffb5fcc1af4da6bf779749fe51e6f
Myohres <77498044+Myohres@users.noreply.github.com>
2022-02-09 18:21:31 +0100

    adding subscription

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,14 +2,15 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {MenuComponent} from "./menu/menu.component";
+import {SubscriptionComponent} from "./subscription/subscription.component";
 
 
 
 
 const routes: Routes = [
   { path: '', component: AuthenticationComponent },
-  { path:'home', component: MenuComponent }
-
+  { path:'home', component: MenuComponent },
+  { path:'subscription', component: SubscriptionComponent }
 ];
 
 @NgModule({
